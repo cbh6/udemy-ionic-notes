6b82fce576f7c1e41e6d5a93d244fa9647217b7b
Cristian Botella <cri.bh6@gmail.com>
2019-10-23 17:22:58 +0200

    Add projects 01 and 02

---
+++ Ejemplos ionic/02-angularbases/src/app/pages/posts/posts.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PostsRoutingModule } from './posts-routing.module';
+import { PostsComponent } from './posts.component';
+import { PostComponent } from './post/post.component';
+
+
+@NgModule({
+  declarations: [PostsComponent, PostComponent],
+  imports: [
+    CommonModule,
+    PostsRoutingModule
+  ]
+})
+export class PostsModule { }
